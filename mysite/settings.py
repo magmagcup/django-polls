8ca18882c186167cdaedd6227de477351d3c73f3
Sirawich Direkwattanachai <47655424+magmagcup@users.noreply.github.com>
2019-09-16 17:26:05 +0700

    merge externalize config to master, imporve security in code. (#1)
    * Fix text
    
    * decouple secret key and debug in setting.py
    
    * Add how to access a polls aplication, fix some text

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
+import dj_database_url as ab_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,13 +22,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a)-hmtq)5*^x+ud$rgu-)a4r!@*k6p0($3=5_+h_1mgg32^s9q'
+SECRET_KEY = config('SECRET_KEY', default="secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
+DEBUG = config('DEBUG', cast=bool)
+
+#Original
+# ALLOWED_HOSTS = []
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
