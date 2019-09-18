949a734af0e0f1da9ecd4932f487e339058adb51
magmagcup <47655424+magmagcup@users.noreply.github.com>
2019-09-18 23:42:16 +0700

    Use dj_database_url for clean database URLs.

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,6 @@ SECRET_KEY = config('SECRET_KEY', default="secret")
 DEBUG = config('DEBUG', cast=bool)
 
 #Original
-# ALLOWED_HOSTS = []
 ALLOWED_HOSTS = []
 
 # Application definition
@@ -78,13 +77,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+DATABASES = {'default': ab_url.config(default='sqlite:///db.sqlite3')}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
