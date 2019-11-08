f68191d35d5b51e7f47f73f8c950013a7a661136
Sirawich Direkwattanachai <47655424+magmagcup@users.noreply.github.com>
2019-11-09 04:13:42 +0700

    One vote per person (#5)
    * add model and require login to vote
    
    * Add vote object after eachvote
    
    * One vote per person

--- mysite/urls.py
+++ mysite/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', views.signup, name="signup"),
     path('', views.IndexView.as_view()),
 ]
--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 
 
@@ -31,3 +32,17 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+class Vote(models.Model):
+    """A vote by a user for a particular choice in a Question"""
+    vote_question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def change_vote(self, new_choice):
+        for each_choice in [self.choice]:
+            each_choice.votes -= 1
+            each_choice.save()
+        self.choice = new_choice
+        self.save()
