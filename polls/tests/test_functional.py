8f71f36bff0fe744897a3ba97ff2ec59e364438f
magmagcup <47655424+magmagcup@users.noreply.github.com>
2019-12-02 00:35:38 +0700

    Finish all Test case

--- polls/tests/test_functional.py
+++ polls/tests/test_functional.py
@@ -2,8 +2,12 @@ import datetime
 from typing import List
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.common.exceptions import WebDriverException
-from polls.views import *
+from django.contrib.auth.models import User
+from django.test.client import Client
 from polls.models import Question
+from polls.models import Choice
+from django.utils import timezone
+
 import requests
 
 from django.test import LiveServerTestCase
@@ -17,12 +21,14 @@ class FunctionTest(StaticLiveServerTestCase):
         :return:
         """
         super(FunctionTest, cls).setUpClass()
+
         try:
             cls.browser = WebDriver()
         except WebDriverException:
             cls.browser = WebDriver(executable_path='E:/chrome_driver/chromedriver.exe')
         finally:
             cls.browser.implicitly_wait(10)
+
 
     @classmethod
     def tearDownClass(cls):
@@ -33,8 +39,16 @@ class FunctionTest(StaticLiveServerTestCase):
         """
         :return:
         """
-        super().setUp()
         self.web = FunctionTest.browser
+        self.user = Client()
+        Question.objects.create(question_text="Test?", pub_date=timezone.now())
+        Choice.objects.create(question=Question.objects.all()[0], choice_text='TEST!')
+        User.objects.create_user(username='Tester', email='tester@hotmail.com', password='test')
+        self.user.login(username='Tester', password='test')
+        user_cookie = self.user.cookies['sessionid']
+        self.web.get('%s' % self.live_server_url)
+        self.web.add_cookie({'name': 'sessionid', 'value': user_cookie.value})
+        super().setUp()
 
     def test_find_h1(self):
         self.web.get('%s' % self.live_server_url)
@@ -43,13 +57,20 @@ class FunctionTest(StaticLiveServerTestCase):
 
     def test_find_question(self):
         self.web.get('%s' % self.live_server_url)
-        text_question = self.web.find_element_by_tag_name('p')
-        self.assertEqual(text_question.text, "No polls are available.")
+        question = self.web.find_element_by_id('question1')
+        self.assertEqual(question.text, "Test?")
 
-    def test_hyperlink(self):
+    def test_to_vote(self):
         self.web.get('%s' % self.live_server_url)
-        self.web.find_element_by_link_text('login').click()
-        self.assertEqual(self.web.current_url, '%s%s' % (self.live_server_url, 'accounts/login/'))
+        self.web.find_element_by_id('question1').click()
+        self.assertURLEqual(self.web.current_url, '%s%s' %
+                            (self.live_server_url, f'/polls/{Question.objects.all()[0].id}/'))
 
     def test_first_choice(self):
-        pass
+        self.web.get('%s%s' % (self.live_server_url, f'/polls/{Question.objects.all()[0].id}/'))
+        self.web.find_element_by_id('choice1').click()
+        self.web.find_element_by_id('submit_button').click()
+
+        self.assertURLEqual(self.web.current_url, self.live_server_url +
+                            f'/polls/{Question.objects.all()[0].id}/results/')
+
